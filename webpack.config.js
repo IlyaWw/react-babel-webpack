846e5680fdac93d4ada45e5a907e92f4d52a48c9
IlyaWw <bjnhks@gmail.com>
2020-02-09 23:39:31 +0300

    + postcss

--- webpack.config.js
+++ webpack.config.js
@@ -11,9 +11,35 @@ module.exports = {
       },
       {
         test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+        },
+      },
+      {
+        test: /\.css$/i,
         use: [
+          { loader: 'style-loader' },
           {
-            loader: 'html-loader',
+            loader: 'css-loader',
+            options: {
+              modules: {
+                mode: 'local',
+                localIdentName: '[path][local]__[hash:base64:5]',
+              },
+              localsConvention: 'camelCase',
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: loader => [
+                require('postcss-preset-env')(),
+                require('postcss-nested')(),
+                require('postcss-flexbugs-fixes')(),
+              ],
+            },
           },
         ],
       },
@@ -21,8 +47,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./public/index.html",
-      filename: "./index.html"
-    })
-  ]
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+  ],
 };
